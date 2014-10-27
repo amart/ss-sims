8127c79e1b0adb02fdddcc5a327cee2f79262257
amart <amart@users.noreply.github.com>
2014-10-27 15:58:36 -0700

    Parallelized

--- R/run_sims.R
+++ R/run_sims.R
@@ -10,6 +10,9 @@
 devtools::install_github("amart/r4ss")
 library(r4ss)
 
+
+library(foreach)
+library(doParallel)
 library(Hmisc)
 library(stringr)
 
@@ -21,7 +24,12 @@ seas <- 1
 index_fleet <- 4
 
 
-source_dir  <- "\\home\\w\\dev\\SS sims\\R"
+
+# ~~~~~~~~~~~ variables set for specific simulations ~~~~~~~~~~~
+
+# num of CPUs for projections running in parallel
+num_cpus <- 4
+
 
 working_dir <- "\\home\\w\\dev\\SS sims\\run"
 
@@ -44,21 +52,21 @@ index_data <- read.csv(idx_file,header=TRUE,numerals="no.loss")
 dim(index_data)
 
 nrows_data <- dim(index_data)[1]
-num_indices <- (dim(index_data)[2]) - 1
+num_indices <- (dim(index_data)[2]) - 1     # account for the year in the first column
 index_names <- names(index_data)[-1]
 
 
 if (nrows_data < 1 || num_indices < 1)
 {
-    print("Error with index file")
+    cat("Error with index file\n")
     return(1)
 }
 
 
 # number of projections for each index; limited to max of 99
-num_proj <- min(min(99,num_proj_years),nrows_data)
+num_proj_years <- min(min(99,num_proj_years),nrows_data)
 
-
+cat(paste("\nNumber of projection years for each index:  ",num_proj_years,"\n\n",sep=""))
 
 
 
@@ -74,13 +82,12 @@ copy_sim_files_into_dir <- function(copy_to_dir)
 
 
 
+start_struct <- SS_readstarter(starter_file)
+dat_struct   <- SS_readdat(dat_file)
+ctl_struct   <- readLines(paste(ctl_file,".for_proj",sep=""))   # will need to update if and when SS_readctl() and SS_writectl() are completed
+fc_struct    <- SS_readforecast(forecast_file,Nfleets=dat_struct$Nfleet,Nareas=dat_struct$N_areas)
 
-dat_struct <- SS_readdat(dat_file)
-ctl_struct <- readLines(paste(ctl_file,".for_proj",sep=""))   # will need to update if and when SS_readctl() and SS_writectl() are completed
-fc_struct  <- SS_readforecast(forecast_file,Nfleets=dat_struct$Nfleet,Nareas=dat_struct$N_areas)
 
-# not necessary for now
-# start_struct <- SS_readstarter(starter_file)
 
 
 
@@ -112,108 +119,148 @@ if (!is.null(base_catch_proj))
 } else {
     base_catch_tot  <- 0.0
     base_catch_frac <- matrix(0.0,nrow=dat_struct$nseas,ncol=dat_struct$Nfleet)
-    print("Warning:  bad forecasted catch in base run")
+    cat("\nWarning:  bad forecasted catch in base run\n\n")
 }
 
 
 
-# main loop for projections
-for (i in 1:num_indices)
+
+
+# ~~~~~~~~~~~ function to run projections for a specific index number ~~~~~~~~~~~
+do_projections_for_index <- function(index_num=-1)
 {
-    idx_name <- str_trim(index_names[i],side="both")
-    index_names[i] <- idx_name
+    library(r4ss)
+    library(Hmisc)
+    library(stringr)
 
-    new_dat_struct <- dat_struct
-    new_ctl_struct <- ctl_struct
-    new_fc_struct  <- fc_struct
-
-    catch_year <- dat_struct$endyr
-    catch_tot  <- base_catch_tot
-    catch_frac <- base_catch_frac
-
-    for (j in 1:num_proj)
+    if (index_num >= 1 && index_num <= num_indices)
     {
-        setwd(working_dir)
+        idx_name <- str_trim(index_names[index_num],side="both")
+        index_names[index_num] <- idx_name
 
-        if (j > 1)
+        cat(paste("\nStarting projections for index ",idx_name,"\n\n",sep=""))
+
+        new_dat_struct <- dat_struct
+        new_ctl_struct <- ctl_struct
+        new_fc_struct  <- fc_struct
+
+        catch_year <- dat_struct$endyr
+        catch_tot  <- base_catch_tot
+        catch_frac <- base_catch_frac
+
+        for (j in 1:num_proj_years)
         {
-            # save the previous directory if this is not the first run for this index
-            prev_dir <- new_dir
+            setwd(working_dir)
 
-            # get catch by fleet from previous year's Forecast-report.sso file
-            catch_proj <- sim_get_forecast_catch_by_fleet(prev_dir,dat_struct$Nfleet,dat_struct$nseas,fc_struct$Nforecastyrs)
-
-            if (!is.null(catch_proj))
+            if (j > 1)
             {
-                catch_tot  <- sum(catch_proj)
-                catch_frac <- catch_proj / sum(catch_proj)
-            } else {
-                catch_tot  <- 0.0
-                catch_frac <- matrix(0.0,nrow=dat_struct$nseas,ncol=dat_struct$Nfleet)
-                print(paste("Warning:  bad forecasted catch in index ",i," run ",(j-1),sep=""))
+                # save the previous directory if this is not the first run for this index
+                prev_dir <- new_dir
+
+                # get catch by fleet from previous year's Forecast-report.sso file
+                catch_proj <- sim_get_forecast_catch_by_fleet(prev_dir,dat_struct$Nfleet,dat_struct$nseas,fc_struct$Nforecastyrs)
+
+                if (!is.null(catch_proj))
+                {
+                    catch_tot  <- sum(catch_proj)
+                    catch_frac <- catch_proj / sum(catch_proj)
+                } else {
+                    catch_tot  <- 0.0
+                    catch_frac <- matrix(0.0,nrow=dat_struct$nseas,ncol=dat_struct$Nfleet)
+                    cat(paste("\nWarning:  bad forecasted catch in index ",idx_name," run ",(j-1),"\n\n",sep=""))
+                }
             }
+
+            new_dir <- paste(idx_name,"\\",sprintf("%02d",j),sep="")
+            dir.create(new_dir,recursive=TRUE)
+
+            # copy files from current directory into directory for initial run
+            copy_sim_files_into_dir(new_dir)
+
+
+            setwd(new_dir)
+
+
+            curr_year  <- catch_year
+            catch_year <- catch_year + 1
+
+
+            # edit the DAT file
+            new_dat_struct <- sim_set_endyr(new_dat_struct,catch_year)
+
+            # calculate fraction of total annual catch for each fleet and add to catch
+            catch_mat <- catch_tot * catch_frac
+            new_dat_struct <- sim_add_catch(new_dat_struct,catch_mat,catch_year)
+
+            # add index for endyr to CPUE
+            idx_yr <- which(index_data$Year == new_dat_struct$endyr,arr.ind=TRUE)
+            if (idx_yr > 0)
+            {
+                new_dat_struct <- sim_add_index_as_CPUE(new_dat_struct,catch_year,seas,index_fleet,index_data[idx_yr,(index_num+1)],std_err)
+            }
+
+            SS_writedat(new_dat_struct,dat_file,overwrite=TRUE,verbose=TRUE)
+
+
+            # edit the forecast file
+            new_fc_struct$FirstYear_for_caps_and_allocations <- catch_year + 1
+
+            SS_writeforecast(new_fc_struct,file=forecast_file,overwrite=TRUE,verbose=TRUE)
+
+
+            # edit the CTL file
+            # change catch_year to catch_year+1 in bias adjustment section
+            new_ctl_struct <- sedit(new_ctl_struct,as.character(catch_year),as.character(catch_year+1))
+
+            # change curr_year to curr_year+1 in bias adjustment section
+            new_ctl_struct <- sedit(new_ctl_struct,as.character(curr_year),as.character(catch_year))
+
+            writeLines(new_ctl_struct,ctl_file)
+
+
+            system(paste(ss_exe_file,"-nox",sep=" "),intern=FALSE,ignore.stderr=TRUE,wait=TRUE,
+                   input=NULL,show.output.on.console=FALSE,minimized=FALSE,invisible=FALSE)
+
+
+            setwd(working_dir)
+
         }
 
-        new_dir <- paste(idx_name,"\\",sprintf("%02d",j),sep="")
-        dir.create(new_dir,recursive=TRUE)
-
-        # copy files from current directory into directory for initial run
-        copy_sim_files_into_dir(new_dir)
-
-
-        setwd(new_dir)
-
-
-        curr_year  <- catch_year
-        catch_year <- catch_year + 1
-
-
-        # edit the DAT file
-        new_dat_struct <- sim_set_endyr(new_dat_struct,catch_year)
-
-        # calculate fraction of total annual catch for each fleet and add to catch
-        catch_mat <- catch_tot * catch_frac
-        new_dat_struct <- sim_add_catch(new_dat_struct,catch_mat,catch_year)
-
-        # add index for endyr to CPUE
-        idx_yr <- which(index_data$Year == new_dat_struct$endyr,arr.ind=TRUE)
-        if (idx_yr > 0)
-        {
-            new_dat_struct <- sim_add_index_as_CPUE(new_dat_struct,catch_year,seas,index_fleet,index_data[idx_yr,(i+1)],std_err)
-        }
-
-        SS_writedat(new_dat_struct,dat_file,overwrite=TRUE,verbose=TRUE)
-
-
-        # edit the forecast file
-        new_fc_struct$FirstYear_for_caps_and_allocations <- catch_year + 1
-
-        SS_writeforecast(new_fc_struct,file=forecast_file,overwrite=TRUE,verbose=TRUE)
-
-
-        # edit the CTL file
-        # change catch_year to catch_year+1 in bias adjustment section
-        new_ctl_struct <- sedit(new_ctl_struct,as.character(catch_year),as.character(catch_year+1))
-
-        # change curr_year to curr_year+1 in bias adjustment section
-        new_ctl_struct <- sedit(new_ctl_struct,as.character(curr_year),as.character(catch_year))
-
-        writeLines(new_ctl_struct,ctl_file)
-
-
-        system(paste(ss_exe_file,"-nox",sep=" "),intern=FALSE,ignore.stderr=TRUE,wait=TRUE,
-               input=NULL,show.output.on.console=FALSE,minimized=FALSE,invisible=FALSE)
-
-
-        setwd(working_dir)
-
+        cat(paste("\nCompleted projections for index ",idx_name,"\n\n",sep=""))
     }
 }
+
+
+
+
+
+# set up doParallel for running in parallel
+loop_cl <- makeCluster(num_cpus)
+registerDoParallel(loop_cl)
+
+cat(paste("\nNumber of CPUs in use:  ",getDoParWorkers(),"\n\n",sep=""))
+
+
+setwd(working_dir)
+
+
+
+# ~~~~~~~~~~~ main loop for projections ~~~~~~~~~~~
+foreach (i = 1:num_indices,.export=c("index_names","num_indices","working_dir","dat_struct","ctl_struct","fc_struct","base_catch_tot","base_catch_frac")) %dopar% do_projections_for_index(i)
+
+
+
+stopCluster(loop_cl)
+
+
+
+cat(paste("\nCompleted projections\n\n",sep=""))
+
 
 
 setwd(working_dir)
 
 # check output
-new_run <- SS_output(new_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE)
-SS_plots(new_run)
+# new_run <- SS_output(new_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE)
+# SS_plots(new_run)
 
