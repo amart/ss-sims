f62b63c4de30dd1b72f90a1a33cc22ba9c726486
amart <amart@users.noreply.github.com>
2015-01-12 15:01:03 -0800

    Do a calc-only run before the main run
    Do a no-estimation/calculation-only run with dummy length comp data to calculate the expected length comp data and the population numbers-at-length in order to generate the length comp data before doing the main run for each index and year.

--- R/run_sims.R
+++ R/run_sims.R
@@ -39,11 +39,20 @@ index_fleet <- 4
 
 
 num_proj_fleets <- 4
-proj_seas <- c(1,1,1,1)
 proj_fleet <- c(1,2,3,8)
+proj_fleet_seas <- c(1,1,1,1)
+proj_fleet_name <- c("HKL","POT","TWL","NWCBO")
+proj_fleet_len_comp_gender <- c(0,0,0,3)
+proj_fleet_len_comp_part <- c(2,2,2,0)
+proj_fleet_age_comp_gender <- c(3,3,3,3)
+proj_fleet_age_comp_part <- c(2,2,2,0)
+
+
 
 
 working_dir <- "\\home\\w\\dev\\SS sims\\run"
+
+
 
 
 # ~~~~~~~~~~~ variables set for specific simulations ~~~~~~~~~~~
@@ -55,9 +64,13 @@ num_cpus <- 4
 ss_exe_file <- "ss3.exe"
 
 starter_file <- "starter.ss"
+starter_calc_only_file <- paste(starter_file,".for_proj",sep="")
 forecast_file <- "forecast.ss"
-dat_file <- "DAT.ss"
-ctl_file <- "CTL.ss"
+dat_file <- "2011_sablefish_data.ss"
+ctl_file <- "2011_sablefish_control.ss"
+est_dat_file <- "data.ss_new"
+
+par_file <- "ss3.par"
 
 idx_file <- "indices.csv"
 idx_std_err_file <- "indices.std_err.csv"
@@ -103,14 +116,45 @@ cat(paste("\nNumber of projection years for each index:  ",num_proj_years,"\n\n"
 
 
 
-copy_sim_files_into_dir <- function(copy_to_dir)
+
+paste_dir_file <- function(paste_dir,paste_filename)
 {
-    file.copy(ss_exe_file,copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
-    file.copy(starter_file,copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
-    file.copy(forecast_file,copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
-    file.copy(dat_file,copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
-    file.copy(ctl_file,copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    return(paste(paste_dir,"\\",paste_filename,sep=""))
 }
+
+
+
+copy_sim_files_into_dir <- function(copy_from_dir,copy_to_dir)
+{
+    file.copy(paste_dir_file(copy_from_dir,ss_exe_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,starter_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,forecast_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,dat_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,ctl_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+}
+
+
+
+copy_sim_calc_only_files_into_dir <- function(copy_from_dir,copy_to_dir)
+{
+    file.copy(paste_dir_file(copy_from_dir,ss_exe_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,starter_calc_only_file),paste_dir_file(copy_to_dir,starter_file),copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,forecast_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,dat_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,ctl_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE)
+}
+
+
+
+copy_sim_run_files_into_dir <- function(copy_from_dir,copy_to_dir)
+{
+    file.copy(paste_dir_file(copy_from_dir,dat_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE,overwrite=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,ctl_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE,overwrite=TRUE)
+    file.copy(paste_dir_file(copy_from_dir,forecast_file),copy_to_dir,copy.mode=TRUE,copy.date=TRUE,overwrite=TRUE)
+}
+
+
+
 
 
 
@@ -128,7 +172,7 @@ fc_struct    <- SS_readforecast(forecast_file,Nfleets=dat_struct$Nfleet,Nareas=d
 base_dir <- "00"
 dir.create(base_dir)
 
-copy_sim_files_into_dir(base_dir)
+copy_sim_files_into_dir(working_dir,base_dir)
 
 
 setwd(base_dir)
@@ -140,7 +184,10 @@ system(paste(ss_exe_file,"-nox",sep=" "),intern=FALSE,ignore.stderr=TRUE,wait=TR
 setwd(working_dir)
 
 # get the results of the base run
-base_rep_struct <- SS_output(base_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE)
+base_rep_struct <- SS_output(base_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE,covar=FALSE)
+
+# get the estimated parameters from the base run
+base_par_struct <- readLines(paste_dir_file(base_dir,par_file))
 
 # get the forecasted catch by fleet
 base_catch_proj <- sim_get_forecast_catch_by_fleet(base_dir,dat_struct$Nfleet,dat_struct$nseas,fc_struct$Nforecastyrs)
@@ -159,21 +206,55 @@ if (!is.null(base_catch_proj))
 
 
 
-# get the mean index std err, length, and age sample sizes for the projected fleets
-proj_fleet_std_err    <- rep(0.0,num_proj_fleets)
-proj_fleet_len_comp_N <- rep(0.0,num_proj_fleets)
-proj_fleet_age_comp_N <- rep(0.0,num_proj_fleets)
+# get the mean index std err, length and age sample sizes, and discard std err for the projected fleets
+proj_fleet_cpue_std_err    <- rep(0.0,num_proj_fleets)
+proj_fleet_len_comp_N      <- rep(0.0,num_proj_fleets)
+proj_fleet_age_comp_N      <- rep(0.0,num_proj_fleets)
+proj_fleet_discard_average <- rep(0.0,num_proj_fleets)
+proj_fleet_discard_std_err <- rep(0.0,num_proj_fleets)
 
 for (f in 1:num_proj_fleets)
 {
     proj_fl <- proj_fleet[f]
 
-    proj_fleet_std_err[f]    <- max(0.01,mean(dat_struct$CPUE[which(dat_struct$CPUE$index == proj_fl,arr.ind=TRUE),]$se_log))
+    proj_fleet_cpue_std_err[f] <- max(0.01,mean(dat_struct$CPUE[which(dat_struct$CPUE$index == proj_fl,arr.ind=TRUE),]$se_log))
 
-    proj_fleet_len_comp_N[f] <- max(100,floor(mean(dat_struct$lencomp[which(dat_struct$lencomp$FltSvy == proj_fl,arr.ind=TRUE),]$Nsamp)))
+    proj_fleet_len_comp_N[f]   <- min(400,max(100,floor(mean(dat_struct$lencomp[which(dat_struct$lencomp$FltSvy == proj_fl,arr.ind=TRUE),]$Nsamp))))
 
-    proj_fleet_age_comp_N[f] <- max(50,floor(mean(dat_struct$agecomp[which(dat_struct$agecomp$FltSvy == proj_fl,arr.ind=TRUE),]$Nsamp)))
+    proj_fleet_age_comp_N[f]   <- min(400,max(50,floor(mean(dat_struct$agecomp[which(dat_struct$agecomp$FltSvy == proj_fl,arr.ind=TRUE),]$Nsamp))))
+
+    if (dat_struct$N_discard_fleets > 0 && f %in% dat_struct$discard_fleet_info$Fleet)
+    {
+        proj_fleet_discard_average[f] <- 0.0
+        proj_fleet_discard_std_err[f] <- 0.001
+
+        fleet_discard <- subset(dat_struct$discard_data,dat_struct$discard_data$Flt == f)
+        nrows_discard <- nrow(fleet_discard)
+
+        if (nrows_discard > 0)
+        {
+            # get the average of the most recent 5 values
+            if (nrows_discard > 5)
+            {
+                exclude_idx <- nrows_discard - 5
+                obs_avg     <- mean(fleet_discard$Discard[-exclude_idx:-1])
+                std_err_avg <- mean(fleet_discard$Std_in[-exclude_idx:-1])
+            } else {
+                obs_avg     <- mean(fleet_discard$Discard)
+                std_err_avg <- mean(fleet_discard$Std_in)
+            }
+
+            proj_fleet_discard_average[f] <- obs_avg
+            proj_fleet_discard_std_err[f] <- max(0.001,std_err_avg)
+
+            # there are no discard obs for 2010, so add them
+            dat_struct <- sim_add_discard(dat_struct,dis_year=2010,dis_seas=proj_fleet_seas[f],dis_fleet=proj_fleet[f],dis_obs=proj_fleet_discard_average[f],dis_std_err=proj_fleet_discard_std_err[f])
+        }
+    }
 }
+
+
+
 
 
 
@@ -202,6 +283,7 @@ do_projections_for_index <- function(index_num=-1)
         catch_tot  <- base_catch_tot
         catch_frac <- base_catch_frac
 
+        prev_par_struct <- base_par_struct
         prev_rep_struct <- base_rep_struct
 
         for (j in 1:num_proj_years)
@@ -214,7 +296,10 @@ do_projections_for_index <- function(index_num=-1)
                 prev_dir <- new_dir
 
                 # get the results from the previous run
-                prev_rep_struct <- SS_output(prev_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE)
+                prev_rep_struct <- SS_output(prev_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE,covar=FALSE)
+
+                # get the estimated parameters from the previous run
+                prev_par_struct <- readLines(paste_dir_file(prev_dir,par_file))
 
                 # get catch by fleet from previous year's Forecast-report.sso file
                 catch_proj <- sim_get_forecast_catch_by_fleet(prev_dir,dat_struct$Nfleet,dat_struct$nseas,fc_struct$Nforecastyrs)
@@ -230,14 +315,18 @@ do_projections_for_index <- function(index_num=-1)
                 }
             }
 
-            new_dir <- paste(idx_name,"\\",sprintf("%02d",j),sep="")
-            dir.create(new_dir,recursive=TRUE)
-
-            # copy files from current directory into directory for initial run
-            copy_sim_files_into_dir(new_dir)
+            new_dir <- paste_dir_file(idx_name,sprintf("%02d",j))
+            new_calc_only_dir <- paste_dir_file(new_dir,"calc_only")
+            dir.create(new_calc_only_dir,recursive=TRUE)
 
 
-            setwd(new_dir)
+            # ||||||||||||||||| THE (length comp) CALCULATION RUN |||||||||||||||||
+
+
+            # copy files from the initial directory for the length comp calculation run
+            copy_sim_calc_only_files_into_dir(working_dir,new_calc_only_dir)
+
+            setwd(new_calc_only_dir)
 
 
             curr_year  <- catch_year
@@ -262,26 +351,17 @@ do_projections_for_index <- function(index_num=-1)
             for (f in 1:num_proj_fleets)
             {
                 proj_fl <- proj_fleet[f]
-                proj_ss <- proj_seas[f]
-                proj_se <- proj_fleet_std_err[f]
+                proj_ss <- proj_fleet_seas[f]
+                proj_se <- proj_fleet_cpue_std_err[f]
 
                 # add generated srv index for endyr to CPUE for proj_fleet
                 if (is.numeric(proj_se) && !is.na(proj_se))
                 {
-                    cpue_struct <- sim_generate_CPUE(new_dat_struct,prev_rep_struct,catch_year,proj_ss,proj_fl,apply_error=FALSE)
+                    cpue_struct <- sim_generate_CPUE(new_dat_struct,prev_rep_struct,catch_year,proj_ss,proj_fl,proj_se,apply_error=TRUE)
                     if (proj_se > 0.0 && cpue_struct$obs > 0.0)
                     {
                         new_dat_struct <- sim_add_CPUE(new_dat_struct,catch_year,proj_ss,proj_fl,cpue_struct$obs,proj_se)
                     }
-                }
-
-                add generated srv length comps for endyr to lencomp for proj_fleet
-                lencomp_struct <- sim_generate_length_comp(new_dat_struct,prev_rep_struct,catch_year,proj_ss,proj_fl,apply_error=TRUE)
-                lencomp_struct <- sim_map_pop_len_to_data_len(new_dat_struct,lencomp_struct)
-                if (!is.null(lencomp_struct))
-                {
-                    lencomp_struct <- lencomp_struct / sum(lencomp_struct)
-                    new_dat_struct <- sim_add_length_comp(new_dat_struct,catch_year,proj_ss,proj_fl,proj_fleet_len_comp_N[f],lencomp_struct)
                 }
 
                 # add generated srv age comps for endyr to agecomp for proj_fleet
@@ -290,7 +370,33 @@ do_projections_for_index <- function(index_num=-1)
                 if (!is.null(agecomp_struct))
                 {
                     agecomp_struct <- agecomp_struct / sum(agecomp_struct)
-                    new_dat_struct <- sim_add_age_comp(new_dat_struct,catch_year,proj_ss,proj_fl,proj_fleet_age_comp_N[f],agecomp_struct)
+                    new_dat_struct <- sim_add_age_comp(new_dat_struct,catch_year,proj_ss,proj_fl,proj_fleet_age_comp_N[f],proj_fleet_age_comp_gender[f],proj_fleet_age_comp_part[f],agecomp_struct)
+                }
+
+                # add average of historical values for discard for proj_fleet
+                if (new_dat_struct$N_discard_fleets > 0 && f %in% new_dat_struct$discard_fleet_info$Fleet)
+                {
+                    new_dat_struct <- sim_add_discard(new_dat_struct,dis_year=catch_year,dis_seas=proj_fleet_seas[f],dis_fleet=proj_fleet[f],dis_obs=proj_fleet_discard_average[f],dis_std_err=proj_fleet_discard_std_err[f])
+                }
+            }
+
+            # save the DAT file struct; without the length comp data, it is the same for the calc-only run and the main run
+            save_new_dat_struct <- new_dat_struct
+
+            # generate dummy length comp "data" for the projected fleets
+            for (f in 1:num_proj_fleets)
+            {
+                proj_fl <- proj_fleet[f]
+                proj_ss <- proj_fleet_seas[f]
+
+                # add generated length comps for endyr to lencomp for proj_fleet
+                lencomp_struct <- sim_generate_length_comp(new_dat_struct,prev_rep_struct,catch_year,proj_ss,proj_fl,apply_error=FALSE)
+                lencomp_struct <- sim_map_pop_len_to_data_len(new_dat_struct,lencomp_struct)
+                if (!is.null(lencomp_struct))
+                {
+                    # these values are placeholder/dummy values; they will NOT be used in model fitting
+                    lencomp_struct <- lencomp_struct / sum(lencomp_struct)
+                    new_dat_struct <- sim_add_length_comp(new_dat_struct,catch_year,proj_ss,proj_fl,1,proj_fleet_len_comp_gender[f],proj_fleet_len_comp_part[f],lencomp_struct)
                 }
             }
 
@@ -316,7 +422,74 @@ do_projections_for_index <- function(index_num=-1)
             writeLines(new_ctl_struct,ctl_file)
 
 
-            # run it
+            # &&&&&&&&&&&&&&&&& edit the input PAR file &&&&&&&&&&&&&&&&&
+            new_par_struct <- prev_par_struct
+
+            # on which line are the main rec devs (name recdev1)?
+            rec_dev_line <- 1 + grep("# recdev1:",prev_par_struct)
+
+            # add " 0.0" to the main rec devs vector
+            new_par_struct[rec_dev_line] <- paste(prev_par_struct[rec_dev_line]," 0.0",sep="")
+
+            # ----------------- write the new input PAR file -----------------
+            writeLines(new_par_struct,par_file)
+
+
+            # run the calculations only run
+            system(paste(ss_exe_file,"-nox","-nohess",sep=" "),intern=FALSE,ignore.stderr=TRUE,wait=TRUE,
+                   input=NULL,show.output.on.console=FALSE,minimized=FALSE,invisible=FALSE)
+
+
+
+
+            # ||||||||||||||||| THE MAIN RUN |||||||||||||||||
+
+            # (currently in new_calc_only_dir)
+            # get the calculated DAT file values from the calculation only run
+            calc_only_dat_struct <- SS_readdat(est_dat_file,section=2)
+
+            # get the calculated REP file values from the calculation only run
+            calc_only_rep_struct <- SS_output(".",forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE,covar=FALSE)
+
+
+            setwd(working_dir)
+
+
+            # copy files from the initial directory into this directory for the main run
+            copy_sim_files_into_dir(working_dir,new_dir)
+
+            # copy DAT, CTL, and forecast files from the calculation only run to this directory
+            copy_sim_run_files_into_dir(new_calc_only_dir,new_dir)
+
+
+            setwd(new_dir)
+
+
+            # &&&&&&&&&&&&&&&&& edit the DAT file &&&&&&&&&&&&&&&&&
+            # new_dat_struct <- SS_readdat(dat_file)    # the new DAT file is already in memory
+            new_dat_struct <- save_new_dat_struct
+
+            # generate "data" for the projected fleets given the calc-only run output (REP) values
+            for (f in 1:num_proj_fleets)
+            {
+                proj_fl <- proj_fleet[f]
+                proj_ss <- proj_fleet_seas[f]
+
+                # add generated length comps for endyr to lencomp for proj_fleet
+                lencomp_struct <- sim_generate_length_comp(new_dat_struct,calc_only_rep_struct,catch_year,proj_ss,proj_fl,apply_error=TRUE)
+                lencomp_struct <- sim_map_pop_len_to_data_len(new_dat_struct,lencomp_struct)
+                if (!is.null(lencomp_struct))
+                {
+                    lencomp_struct <- lencomp_struct / sum(lencomp_struct)
+                    new_dat_struct <- sim_add_length_comp(new_dat_struct,catch_year,proj_ss,proj_fl,proj_fleet_len_comp_N[f],proj_fleet_len_comp_gender[f],proj_fleet_len_comp_part[f],lencomp_struct)
+                }
+            }
+
+            # ----------------- write the new DAT file -----------------
+            SS_writedat(new_dat_struct,dat_file,overwrite=TRUE,verbose=TRUE)
+
+
+            # run the main run
             system(paste(ss_exe_file,"-nox",sep=" "),intern=FALSE,ignore.stderr=TRUE,wait=TRUE,
                    input=NULL,show.output.on.console=FALSE,minimized=FALSE,invisible=FALSE)
 
@@ -345,7 +518,31 @@ setwd(working_dir)
 
 
 # ~~~~~~~~~~~ main loop for projections ~~~~~~~~~~~
-foreach (i = 1:num_indices,.export=c("index_names","index_seas","index_fleet","num_indices","index_data","index_std_err","num_proj_years","working_dir","dat_struct","ctl_struct","fc_struct","base_catch_tot","base_catch_frac","base_rep_struct","num_proj_fleets","proj_seas","proj_fleet","proj_fleet_name","proj_fleet_std_err","proj_fleet_len_comp_N","proj_fleet_age_comp_N")) %dopar% do_projections_for_index(i)
+foreach (i = 1:num_indices,.export=c("index_names",
+                                     "index_seas",
+                                     "index_fleet",
+                                     "num_indices",
+                                     "index_data",
+                                     "index_std_err",
+                                     "num_proj_years",
+                                     "working_dir",
+                                     "start_struct",
+                                     "dat_struct",
+                                     "ctl_struct",
+                                     "fc_struct",
+                                     "base_catch_tot",
+                                     "base_catch_frac",
+                                     "base_par_struct",
+                                     "base_rep_struct",
+                                     "num_proj_fleets",
+                                     "proj_fleet_seas",
+                                     "proj_fleet",
+                                     "proj_fleet_name",
+                                     "proj_fleet_cpue_std_err",
+                                     "proj_fleet_len_comp_N",
+                                     "proj_fleet_age_comp_N",
+                                     "proj_fleet_discard_average",
+                                     "proj_fleet_discard_std_err")) %dopar% do_projections_for_index(i)
 
 
 
@@ -360,5 +557,5 @@ cat(paste("\nCompleted projections\n\n",sep=""))
 setwd(working_dir)
 
 # check output
-# new_run <- SS_output(new_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE)
+# new_run <- SS_output(new_dir,forecast=TRUE,verbose=FALSE,printstats=FALSE,hidewarn=TRUE,covar=FALSE)
 # SS_plots(new_run)
